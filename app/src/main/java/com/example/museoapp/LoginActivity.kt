36acad78b1d792c6d124dd290d5e27c04296f94f
Marcos <markensi9@outlook.com>
2022-10-28 14:21:24 +0100

    Se añade un estilo provisional cumpliendo con las guías de diseño de Android.
    Se añaden los esquemas de colores provisionales en el fichero colors.
    Se añaden los textos provisionales en el fichero strings.
    Se añade los SDK de Google para Firebase
    
    *No implementado*
    Falta la funcionalidad del login
    Falta la lógica de Firebase

---
+++ app/src/main/java/com/example/museoapp/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.museoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.OnBackPressedCallback
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
