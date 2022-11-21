6f5975d0a905547ba76643144916653867027fab
Marcos <markensi9@outlook.com>
2022-11-21 18:22:53 +0000

    -Se añade el diseño de fragment_information.xml

--- app/src/main/java/com/example/museoapp/ui/user/UserFragment.kt
+++ app/src/main/java/com/example/museoapp/ui/user/UserFragment.kt
@@ -5,16 +5,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import android.widget.Toast
-import androidx.activity.viewModels
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentTransaction
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.museoapp.*
-import com.example.museoapp.ViewModel.LoginViewModel
 import com.example.museoapp.databinding.FragmentUserLoginBinding
 
 class UserFragment : Fragment() {
